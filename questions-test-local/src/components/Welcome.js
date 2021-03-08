fdbd09d668496671470ce9f207ec2694081834c5
abhimanyubangaru <abhi.bangaru24@gmail.com>
2021-03-08 10:10:19 -0800

    fixed welcome formatting

--- questions-test-local/src/components/Welcome.js
+++ questions-test-local/src/components/Welcome.js
@@ -3,14 +3,19 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div>
-    <h1>
-      Welcome to the Cate Trivia Website!
-    </h1>
+
       <body>
+        <h1>
+          Welcome to the Cate Trivia Website!
+        </h1>
+        
       In this website, you will be able to answer different questions about the different dorms. <br/>
+
       The navigation bar will remain at the top of the screen at all times. Once inside the game section, please click on
       the different buttons(each has a distinct dorm name) to be taken to different question sets.
+
     <br/><strong> When you are on the last question, the next button will disappear. </strong><br/>
+
       Don't worry, just click on one of the other sets.
 
         <br/>
