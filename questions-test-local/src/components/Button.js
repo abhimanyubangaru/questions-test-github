2a18d9fa6d0b3f883ea14cc5d41f89a68691c6d1
abhimanyubangaru <abhi.bangaru24@gmail.com>
2021-03-07 17:25:53 -0800

    Buttons hover
    Aftering hovering onto either the choices or the next button, the color of the button will change--creating a better user interface.

--- questions-test-local/src/components/Button.js
+++ questions-test-local/src/components/Button.js
@@ -5,21 +5,11 @@ const Button = (props) => {
   return (
     <button
       onClick= {props.onClick}
-      style={{backgroundColor : props.color}}
       className = 'btn'> {props.text}
     </button>
 
   )
 }
 
-Button.defaultProps = {
-  color:'steelblue'
-}
-
-Button.propTypes = {
-  text : PropTypes.string,
-  color : PropTypes.string,
-  onClick: PropTypes.func,
-}
 
 export default Button
